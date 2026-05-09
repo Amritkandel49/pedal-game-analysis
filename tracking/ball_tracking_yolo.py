1916d42d48d78bff48dd17b6e7a690e08c9e0c1a
Amritkandel49 <amritkandel062@gmail.com>
2026-05-09 18:08:23 +0545

    chore: Added notebook file used for yolo traininf ball detection

--- tracking/ball_tracking_yolo.py
+++ tracking/ball_tracking_yolo.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 model = YOLO('model_weights/yolo_ball_track_best.pt')
-result = model.track(source='input/input_video_shortest.mp4', show=True, save=True, conf=0.3, iou=0.5)
+result = model.track(source='input/input_video_shortest.mp4', save=True, conf=0.4, iou=0.4)
 
 
 
-print(result.boxes.xyxy)
+print(result)
