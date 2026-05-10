c16f188f9d38dde6ea63595ca8a90fe2a00de5c6
Amritkandel49 <amritkandel062@gmail.com>
2026-05-10 13:23:44 +0545

    chore: wrapped the ball tracker in class

--- tracking/ball_tracking_yolo.py
+++ tracking/ball_tracking_yolo.py
@@ -1,11 +1,29 @@
+import torch
 from ultralytics import YOLO
-import cv2 as cv
-import numpy as np  
+
+class BallTracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def detect_frames(self,frames):
+        ball_detections = []
+        
+        for frame in frames:
+            ball_dict = self.detect_frame(frame)
+            ball_detections.append(ball_dict)
+        
+        return ball_detections
+
+    def detect_frame(self,frame):
+        results = self.model.track(frame,persist=True)[0]
+
+        ball_dict = {}
+        for box in results.boxes:
+            result = box.xyxy.tolist()[0]
+            ball_dict[1] = result
+        
+        return ball_dict
 
 
-model = YOLO('model_weights/yolo_ball_track_best.pt')
-result = model.track(source='input/input_video_shortest.mp4', save=True, conf=0.4, iou=0.4)
-
-
-
-print(result)
+    
